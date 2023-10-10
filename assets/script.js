b911429bfa8864700dea69ace10ff3b494aa96c9
rpm3 <mancuso.b3@gmail.com>
2023-10-10 09:40:35 -0500

    Set up stored city value to make API call

--- assets/script.js
+++ assets/script.js
@@ -23,12 +23,13 @@ function handleCityInput(event) {
 }
 
 function renderCity() {
-    var city = localStorage.getItem('City');
+    city = localStorage.getItem('City');
     console.log(city);
 }
 
+
 // API call for latitude and longitude from city name
-// var cityURL = 'http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=5&appid=012dcd8608fee5e0f427404e5ffe5eba';
+// var cityURL = 'http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid=012dcd8608fee5e0f427404e5ffe5eba';
 // fetch(cityURL)
 //     .then(function(results) {
 //         return results.json();
