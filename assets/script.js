3b12b1bc10c412895f08ec9a13a01212f8c784f4
rpm3 <mancuso.b3@gmail.com>
2023-10-11 15:03:40 -0500

    Current weather displaying correctly, 5-day forecast display started

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,19 @@
 var searchFormEl = document.querySelector('#search-form');
 var cityInput = document.querySelector('#city-input');
 var msgDiv = document.querySelector('#msg');
+var icon = document.querySelector('#current-icon');
+var temp = document.querySelector('#current-temp');
+var wind = document.querySelector('#current-wind');
+var humidity = document.querySelector('#current-humidity');
+var forecastEl = document.querySelector('#forecast');
 
-
+// Establishes basis to display error message for blank city input held within handleCityInput function
 function displayMessage(type, message) {
     msgDiv.textContent = message;
     msgDiv.setAttribute('class', type);
 }
 
+// Sends error if blank input, begins API calls if city entered
 function handleCityInput(event) {
     event.preventDefault();
 
@@ -35,23 +41,48 @@ function callGeoAPI() {
             console.log(data[0].lat, data[0].lon);
             localStorage.setItem('latitude', data[0].lat);
             localStorage.setItem('longitude', data[0].lon);
-        });
-    // callWeatherAPI();
+        })
+        .then(function () {
+            callWeatherAPI();
+        })
+
 }
 
 
-// API call for weather using latitude and longitude
+// API call for weather using latitude and longitude generated from GeoAPI
 function callWeatherAPI() {
     var latitude = localStorage.getItem('latitude');
     var longitude = localStorage.getItem('longitude');
-    var latlongURL = 'api.openweathermap.org/data/2.5/forecast?lat='+latitude+'&lon='+longitude+'&appid=012dcd8608fee5e0f427404e5ffe5eba';
+    var latlongURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+latitude+'&lon='+longitude+'&appid=012dcd8608fee5e0f427404e5ffe5eba&units=imperial';
     fetch(latlongURL)
         .then(function(results) {
             return results.json();
         })
         .then(function(data) {
             console.log(data);
-        });
+            // Assigns values to current weather data and displays it on the page under current weather
+            var cityName = data.city.name;
+            var currentIcon = data.list[0].weather.icon;
+            var currentTemp = data.list[0].main.temp;
+            var currentWind = data.list[0].wind.speed;
+            var currentHumidity = data.list[0].main.humidity;
+            // localStorage.setItem('currentIcon', currentIcon);
+            // localStorage.setItem('currentTemp', currentTemp);
+            // localStorage.setItem('currentWind', currentWind);
+            // localStorage.setItem('currentHumidity', currentHumidity);
+            icon.innerHTML = cityName + currentIcon;
+            temp.innerHTML = 'Temperature: '+currentTemp+' *F';
+            wind.innerHTML = 'Wind Speed: '+currentWind+' mph';
+            humidity.innerHTML = 'Humidity: '+currentHumidity+'%';
+        
+            // for (i=0; i < 40; i+8) {
+            //     var forecast = document.createElement('li');
+            //     forecast.innerHTML = 'Date: '+data.list[i].dt_txt+'<br/>Temperature: '+data.list[i].main.temp+'<br/>Wind Speed: '+data.list[i].wind.speed+'<br/>Humidity: '+data.list[i].main.humidity;
+            //     forecastEl.append(forecast);
+            // }
+        })
+
+            
 
 }
 
